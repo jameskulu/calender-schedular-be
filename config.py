30b79905560dd4f847c3765901d1f77694f29368
James Kulu <james.kulu@outcodesoftware.com>
2024-06-06 22:35:25 +0545

    Initial Commit

--- config.py
+++ config.py
@@ -2,7 +2,5 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/calendar_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    HOLIDAY_API_KEY = os.environ.get("HOLIDAY_API_KEY")
-    SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
